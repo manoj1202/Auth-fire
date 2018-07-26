b724eaaaae50a070b94746181693c26d7d38e9c2
manoj1202 <manoj.appalaneni@gmail.com>
2018-07-26 09:43:42 -0500

    adding Login Form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React,{Component} from "react"
 import {View, Text} from "react-native"
+// import firebase from "firebase"
 import {Header} from "./components/common"
+import LoginForm from "./components/LoginForm"
+
 
 class App extends Component{
+    // Adding FireBase To  the app
+    // componentWillMount(){
+    //     firebase.initializeApp({/*   firebase data */})
+    // }
     render(){
         return(
             <View>
                 <Header headerText="Authentication" />
-                <Text>An App</Text>
+                <LoginForm />
             </View>
         )
     }
