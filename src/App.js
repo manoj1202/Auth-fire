d0d53ce41b2acb8892c3695bb94bf9c7ae10258a
manoj1202 <manoj.appalaneni@gmail.com>
2018-07-30 16:52:31 -0500

    Adding spinner to logout

--- src/App.js
+++ src/App.js
@@ -6,9 +6,20 @@ import LoginForm from "./components/LoginForm"
 
 
 class App extends Component{
+    state ={
+        loggedIn: false
+    }
     // Adding FireBase To  the app
     // componentWillMount(){
     //     firebase.initializeApp({/*   firebase data */})
+
+    // firebase.auth().onAuthStateChanged((user) => {
+    //  if(user){
+        // this.setState({loggedIn: true});
+        // } else{
+        //     this.setState({loggedIn: false})
+        // }
+    // })
     // }
     render(){
         return(
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -12,7 +12,9 @@ class LoginForm extends Component {
     }
     onButtonPress(){
         const {email, password} = this.state
+
         this.setState({error:"", loading: true})
+        
         firebase.auth().signInWithEmailAndPassword(email, password)
          .then(this.onLoginSuccess.bind(this))
          .catch(() =>{
